cfc245ee2dd508f54a7b4144977b4d535badd71a
Felipe Miranda <felipe@beatsunion.com>
2025-07-29 21:25:46 -0300

    install blip ds package

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,33 @@
-import { StrictMode } from 'react'
+import { StrictMode, type DetailedHTMLProps, type HTMLAttributes } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { defineCustomElements, type JSX as LocalJSX } from 'blip-ds/loader';
+type StencilToReactElements<T = LocalJSX.IntrinsicElements> = {
+  [P in keyof T]?: T[P] &
+    Omit<DetailedHTMLProps<HTMLAttributes<T[P]>, T[P]>, 'className'> & {
+      class?: string;
+    };
+};
+type StencilToReactRef<T = HTMLElementTagNameMap> = {
+  [P in keyof T]: {
+    ref?: DetailedHTMLProps<HTMLAttributes<T[P]>, T[P]>['ref'];
+  };
+};
+type StencilToReact<T = LocalJSX.IntrinsicElements, U = HTMLElementTagNameMap> = StencilToReactElements<T> &
+  StencilToReactRef<U>;
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  export namespace JSX {
+    // eslint-disable-next-line @typescript-eslint/no-empty-interface
+    interface IntrinsicElements extends StencilToReact {}
+  }
+}
+defineCustomElements(window);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
