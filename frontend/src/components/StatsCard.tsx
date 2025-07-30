f7a7abd166e64b5408da23a5084e73aece95f932
Felipe Miranda <felipe@beatsunion.com>
2025-07-30 00:32:17 -0300

    align cards

--- frontend/src/components/StatsCard.tsx
+++ frontend/src/components/StatsCard.tsx
@@ -18,31 +18,34 @@ interface StatCardProps {
 
 export default function StatCard({ title, value, iconName, color = "primary", bgColor }: StatCardProps) {
     return (
-        <div className="h-full">
-            <BdsCard className="h-full w-full" style={{ height: '100%' }}>
-                <BdsCardHeader align="center">
-                    <div className={`flex items-center justify-center w-12 h-12 rounded-full ${bgColor ?? "bg-gray-200"}`}>
-                        <BdsIcon
-                            size="medium"
-                            name={iconName}
-                            theme="outline"
-                            style={{ color: color }} />
-                    </div>
-                    <BdsCardTitle text={title}></BdsCardTitle>
-                </BdsCardHeader>
-                <BdsCardBody>
-                    <BdsGrid
-                        direction="column"
-                        xxs="12"
-                        gap="1"
-                        justify-content="center"
-                        align-items="center"
-                    >
-                        {value}
-                    </BdsGrid>
-                </BdsCardBody>
-                <BdsCardFooter />
-            </BdsCard>
-        </div>
+        <BdsGrid direction="row" justifyContent="flex-start" >
+            <BdsGrid xs="12">
+                <BdsCard 
+                    width="100%">
+                    <BdsCardHeader align="center">
+                        <div className={`flex items-center justify-center w-12 h-12 rounded-full ${bgColor ?? "bg-gray-200"}`}>
+                            <BdsIcon
+                                size="medium"
+                                name={iconName}
+                                theme="outline"
+                                style={{ color: color }} />
+                        </div>
+                        <BdsCardTitle text={title}></BdsCardTitle>
+                    </BdsCardHeader>
+                    <BdsCardBody>
+                        <BdsGrid
+                            direction="column"
+                            xxs="12"
+                            gap="1"
+                            justify-content="center"
+                            align-items="center"
+                        >
+                            {value}
+                        </BdsGrid>
+                    </BdsCardBody>
+                    <BdsCardFooter />
+                </BdsCard>
+            </BdsGrid>
+        </BdsGrid>
     );
 }
