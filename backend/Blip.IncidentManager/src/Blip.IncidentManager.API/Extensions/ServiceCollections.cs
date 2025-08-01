3f03c34fca816ed1ed43f4c2aaed0e581f6a15fa
Felipe Miranda <felipe@beatsunion.com>
2025-08-01 20:16:28 -0300

    add reference from configuration

---
+++ backend/Blip.IncidentManager/src/Blip.IncidentManager.API/Extensions/ServiceCollections.cs
@@ -0,0 +1,14 @@
+﻿using Blip.IncidenteManager.ServiceConfigurations.Extensions;
+
+namespace Blip.IncidentManager.API.Extensions
+{
+    public static class ServiceCollections
+    {
+        public static IServiceCollection AddIncidentManagerApiService(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddServiceConfigurations(configuration);
+
+            return services;
+        }
+    }
+}
