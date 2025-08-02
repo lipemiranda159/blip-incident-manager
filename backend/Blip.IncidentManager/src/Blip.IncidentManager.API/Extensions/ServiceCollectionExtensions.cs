0ef9ecea13509e13ca74e0e5ef041c7b0d80c745
Felipe Miranda <felipe@beatsunion.com>
2025-08-01 23:05:10 -0300

    rename class

---
+++ backend/Blip.IncidentManager/src/Blip.IncidentManager.API/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Blip.IncidentManager.Application._Extensions;
+using Blip.IncidentManager.Persistence.Extensions;
+using Blip.IncidentManager.ServiceConfigurations.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace Blip.IncidentManager.API.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddIncidentManagerApiService(this IServiceCollection services, IConfiguration configuration)
+        {
+            var config = services.AddServiceConfigurations(configuration);
+            services.AddIncidentDbContext(config);
+            services.AddIncidentManagerApplications();
+
+            services.AddAutoMapper(cfg => cfg.AddMaps(typeof(ServiceCollectionExtensions).Assembly));
+
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Incident Manager API",
+                    Version = "v1",
+                    Description = "API sistema de gerenciamento de chamados"
+                });
+            });
+
+            return services;
+        }
+    }
+}
