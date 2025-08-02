8429e7cb08d7693d8b78457f02c28370b9ad4f3c
Felipe Miranda <felipe@beatsunion.com>
2025-08-01 23:05:48 -0300

    add web application extensions

---
+++ backend/Blip.IncidentManager/src/Blip.IncidentManager.API/Extensions/WebApplicationExtensions.cs
@@ -0,0 +1,26 @@
+﻿namespace Blip.IncidentManager.API.Extensions
+{
+    public static class WebApplicationExtensions
+    {
+        public static WebApplication UseIncidentManagerApiSerivce(this WebApplication app)
+        {
+            app.UseHttpsRedirection();         // Redireciona para HTTPS antes de tudo
+
+            app.UseCors("AllowFrontend");      // CORS tem que vir antes da autenticação/autorização
+
+            //app.UseAuthentication();           // Autenticação antes da autorização
+            //app.UseAuthorization();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.RoutePrefix = "swagger";
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "BizCore V1");
+                options.DefaultModelsExpandDepth(-1);
+            });
+
+            app.MapControllers();
+            return app;
+        }
+    }
+}
