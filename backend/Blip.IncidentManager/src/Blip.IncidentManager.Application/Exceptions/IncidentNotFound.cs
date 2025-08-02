3005679022b20c91b8fd65827537344071fdad84
Felipe Miranda <felipe@beatsunion.com>
2025-08-02 09:59:32 -0300

    add incident not found exception

---
+++ backend/Blip.IncidentManager/src/Blip.IncidentManager.Application/Exceptions/IncidentNotFound.cs
@@ -0,0 +1,18 @@
+﻿namespace Blip.IncidentManager.Application.Exceptions
+{
+    [Serializable]
+    internal class IncidentNotFound : Exception
+    {
+        public IncidentNotFound()
+        {
+        }
+
+        public IncidentNotFound(string? message) : base(message)
+        {
+        }
+
+        public IncidentNotFound(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+    }
+}
