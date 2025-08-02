2cf47e45e949bb0bc9e48a2248d0c812f374fdcb
Felipe Miranda <felipe@beatsunion.com>
2025-08-02 11:11:55 -0300

    add get projected

--- backend/Blip.IncidentManager/src/Blip.IncidentManager.Application/Interfaces/IRepository.cs
+++ backend/Blip.IncidentManager/src/Blip.IncidentManager.Application/Interfaces/IRepository.cs
@@ -1,9 +1,16 @@
+using System.Linq.Expressions;
+
 namespace Blip.IncidentManager.Application.Interfaces;
 
 public interface IRepository<T> where T : class
 {
     Task<T?> GetByIdAsync(Guid id);
     Task<IEnumerable<T>> GetAllAsync(int pageNumber, int pageSize, params string[] includes);
+    Task<List<TResult>> GetAllProjectedAsync<TResult>(
+    Expression<Func<T, TResult>> selector,
+    int pageNumber,
+    int pageSize,
+    params string[] includes);
     Task<int> CountAsync();
     Task<T> AddAsync(T entity);
     void Update(T entity);
