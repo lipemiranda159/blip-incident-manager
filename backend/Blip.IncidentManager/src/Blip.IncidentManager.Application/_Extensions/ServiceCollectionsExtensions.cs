6655e4adc48b71ddcab4d3072f835ab634059714
Felipe Miranda <felipe@beatsunion.com>
2025-08-01 23:06:41 -0300

    create service collection extension for application layer

---
+++ backend/Blip.IncidentManager/src/Blip.IncidentManager.Application/_Extensions/ServiceCollectionsExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Blip.IncidentManager.Application._Extensions
+{
+    public static class ServiceCollectionsExtensions
+    {
+        public static IServiceCollection AddIncidentManagerApplications(this IServiceCollection services)
+        {
+            services.AddMediatR(config =>
+            {
+                config.RegisterServicesFromAssemblies(typeof(ServiceCollectionsExtensions).Assembly);
+            });
+
+
+            return services;
+        }
+    }
+}
