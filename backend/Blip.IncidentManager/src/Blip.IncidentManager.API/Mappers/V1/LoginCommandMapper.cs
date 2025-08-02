6af6d7fb9a1390e4f2f7d5230aa2f17e052adbad
Felipe Miranda <felipe@beatsunion.com>
2025-08-02 00:45:34 -0300

    create login command mapper

---
+++ backend/Blip.IncidentManager/src/Blip.IncidentManager.API/Mappers/V1/LoginCommandMapper.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Blip.IncidentManager.Api.ServiceContracts.V1.Request;
+using Blip.IncidentManager.Application.Auth.Commands.Login;
+
+namespace Blip.IncidentManager.API.Mappers.V1
+{
+    public class LoginCommandMapper : Profile
+    {
+        public LoginCommandMapper()
+        {
+            CreateMap<LoginRequest, LoginCommand>();
+        }
+    }
+}
