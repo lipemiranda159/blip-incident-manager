001f12f10fd8518b1d249b02590532d308050a5c
Felipe Miranda <felipe@beatsunion.com>
2025-08-01 23:06:11 -0300

    create mapper for create incident command

---
+++ backend/Blip.IncidentManager/src/Blip.IncidentManager.API/Mappers/V1/CreateIncidentCommandMapper.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Blip.IncidentManager.Api.ServiceContracts.V1.Request;
+using Blip.IncidentManager.Application.Commands;
+
+namespace Blip.IncidentManager.API.Mappers.V1
+{
+    public class CreateIncidentCommandMapper : Profile
+    {
+        public CreateIncidentCommandMapper()
+        {
+            CreateMap<CreateIncidentRequest, CreateIncidentCommand>()
+                .ForMember(dest => dest.Title, opt => opt.MapFrom(src => src.Title))
+                .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.Description));
+        }
+    }
+
+}
