f3ef3adc82c5b619bc7e6df8fb1e85d51eedb992
Felipe Miranda <felipe@beatsunion.com>
2025-08-02 09:58:30 -0300

    create mapper for update command

---
+++ backend/Blip.IncidentManager/src/Blip.IncidentManager.API/Mappers/V1/UpdateIncidentCommandMapper.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Blip.IncidentManager.Api.ServiceContracts.V1.Request;
+using Blip.IncidentManager.Application.Incidents.Commands.Update;
+
+namespace Blip.IncidentManager.API.Mappers.V1
+{
+    public class UpdateIncidentCommandMapper : Profile
+    {
+        public UpdateIncidentCommandMapper()
+        {
+            CreateMap<UpdateIncidentRequest, UpdateIncidentCommand>();
+        }
+    }
+
+}
