fef2788d309799658420dfe0f26a6475c03f1325
Felipe Miranda <felipe@beatsunion.com>
2025-08-04 20:25:23 -0300

    add total count property

--- backend/Blip.IncidentManager/src/Blip.IncidentManager.Application/DTOs/PagedResult.cs
+++ backend/Blip.IncidentManager/src/Blip.IncidentManager.Application/DTOs/PagedResult.cs
@@ -4,6 +4,7 @@ public class PagedResult<T>
 {
     public int CurrentPage { get; set; }
     public int TotalPages { get; set; }
+    public int TotalCount { get; set; }
     public IEnumerable<T>? Items { get; set; } = Enumerable.Empty<T>();
 }
 
