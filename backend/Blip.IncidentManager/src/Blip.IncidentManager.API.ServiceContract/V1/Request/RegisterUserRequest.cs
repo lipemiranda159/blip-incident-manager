50e7ff2c2b5c1f80d5008ba54dcaafe026258d3a
Felipe Miranda <felipe@beatsunion.com>
2025-08-04 23:04:51 -0300

    allow to create user without avatar

--- backend/Blip.IncidentManager/src/Blip.IncidentManager.API.ServiceContract/V1/Request/RegisterUserRequest.cs
+++ backend/Blip.IncidentManager/src/Blip.IncidentManager.API.ServiceContract/V1/Request/RegisterUserRequest.cs
@@ -1,3 +1,3 @@
 ﻿namespace Blip.IncidentManager.Api.ServiceContracts.V1.Request;
 
-public record RegisterUserRequest(string Name, string Email, string Type, string Avatar, string Password);
+public record RegisterUserRequest(string Name, string Email, string Type, string? Avatar, string Password);
